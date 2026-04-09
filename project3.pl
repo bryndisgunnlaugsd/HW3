1ce3b8ba60854dfd184087e97936a4e34a65a5e6
bryndisgunnlaugsd <bryndisg24@ru.is>
2026-04-09 20:06:33 +0000

    loka

--- project3.pl
+++ project3.pl
@@ -68,7 +68,7 @@ never_borrowed(B) :-
     \+ borrowed(_, B).
 
 % True if L is a list of all members (without duplicates)
-member_list(L)
+member_list(L) :-
     findall(M, member(M), All),
     list_to_set(All, L).
 
@@ -78,21 +78,60 @@ member_list(L)
 % ========================
 
 % ---- a. myprefix(L1,L2) ----
+myprefix([], _).
+myprefix([X|T1], [X|T2]) :- myprefix(T1, T2).
 
 % ---- b. num_elements(K,L) ----
+num_elements(0, []).
+num_elements(K, [_|L]) :-
+    num_elements(K1, L),
+    K is K1 + 1.
 
 % ---- c. remove_element(X,L1,L2) ----
+remove_element(X, [X|L1], L1).
+remove_element(X, [Y|L1], [Y|L2]) :- remove_element(X, L1, L2).
 
 % ---- d. insert_element(Elem,L1,Pos,L2) ----
+insert_element(Elem, L1, 1, [Elem|L1]).
+insert_element(Elem, [X|L1], Pos, [X|L2]) :-
+    Pos > 1,
+    Pos1 is Pos - 1,
+    insert_element(Elem, L1, Pos1, L2).
 
 % ---- e. add_up_to(L1,L2)
+add_up_to(L1, L2) :- add_up_to_helper_f(L1, L2, 0).
+ 
+add_up_to_helper_f([], [], _).
+add_up_to_helper_f([X|L1], [S|L2], Acc) :-
+    S is X + Acc,
+    add_up_to_helper_f(L1, L2, S).
 
 % ---- f. remove_duplicates(L1,L2)
+remove_duplicates([], []).
+remove_duplicates([X|L1], L2) :-
+    member(X, L1), !,      
+    remove_duplicates(L1, L2).
+
+remove_duplicates([X|L1], [X|L2]) :- 
+    remove_duplicates(L1, L2).
 
 % ---- g. split_even_odd(L, Even, Odd)
+split_even_odd([], [], []).
+split_even_odd([X], [X], []).
+
+split_even_odd([X,Y|L], [X|Even], [Y|Odd]) :-
+    split_even_odd(L, Even, Odd).
 
 % ---- h. merge_sorted(L1, L2, L3) ----
+merge_sorted([], L2, L2).
+merge_sorted(L1, [], L1).
+merge_sorted([X|L1], [Y|L2], [X|L3]) :-
+    X =< Y,
+    merge_sorted(L1, [Y|L2], L3).
 
+merge_sorted([X|L1], [Y|L2], [Y|L3]) :-
+    X > Y,
+    merge_sorted([X|L1], L2, L3).
 
 
 % ========================
@@ -100,15 +139,33 @@ member_list(L)
 % ========================
 
 % ---- a. binaryTree(T) ----
+binaryTree(nil).
+binaryTree(t(_, L, R)) :-
+    binaryTree(L),
+    binaryTree(R).
 
 % ---- b. preorder(T, L) ----
+preorder(nil, []).
+preorder(t(X, Left, Right), L) :-
+    preorder(Left, L1),
+    preorder(Right, L2),
+    append([X|L1], L2, L).
 
 % ---- c. leaves(T, L) ----
-
+leaves(nil, []).
+leaves(t(X, nil, nil), [X]).
+leaves(t(_, Left, Right), L) :-
+    (Left \= nil ; Right \= nil),
+    leaves(Left, L1),
+    leaves(Right, L2),
+    append(L1, L2, L).
 
 
 % ========================
 %         PART 4
 % ========================
-
-% TODO: Eitthvað PARSING dæmi
+s([]).                                   
+s([0]).                                      
+s([1]).                                
+s(L) :- append([0|M], [0], L), s(M).       
+s(L) :- append([1|M], [1], L), s(M).
