570b5d4a0bee2bde09edee6ca1d20632341c1da5
dagursmari <dagurss24@ru.is>
2026-04-09 22:40:21 +0000

    bug fixes. Everything should work now

--- project3.pl
+++ project3.pl
@@ -1,51 +1,49 @@
-% ========================
-%         PART 1
-% ========================
+% PART 1
 
-% ---- Members ----
+% Members
 member(alice).
 member(bob).
 member(carol).
 member(tom).
 
 
-% ---- Author wrote book ----
+% Author wrote book
 wrote(austen, pride_and_prejudice).
-wrote(tolstoy,  war_and_peace).
-wrote(orwell,   '1984').
-wrote(huxley,   brave_new_world).
-wrote(laxness,  independent_people).
+wrote(tolstoy, war_and_peace).
+wrote(orwell, 1984).
+wrote(huxley, brave_new_world).
+wrote(laxness, independent_people).
 wrote(olafsson, touch).
 
 
-% ----- Book genre -----
+% Book genre
 book_genre(pride_and_prejudice, romance).
-book_genre(war_and_peace,       historical).
-book_genre('1984',              dystopia).
-book_genre(brave_new_world,     dystopia).
-book_genre(independent_people,  fiction).
-book_genre(touch,               fiction).
+book_genre(war_and_peace, historical).
+book_genre(1984, dystopia).
+book_genre(brave_new_world, dystopia).
+book_genre(independent_people, fiction).
+book_genre(touch, fiction).
 
 
-% ----- Borrowing table -----
-% alice  : pride_and_prejudice, '1984', independent_people
-% bob    : war_and_peace, '1984', touch
-% carol  : brave_new_world, touch
-% tom    : pride_and_prejudice, independent_people
+% Borrowing table
+% alice : pride_and_prejudice, 1984, independent_people
+% bob : war_and_peace, 1984, touch
+% carol : brave_new_world, touch
+% tom : pride_and_prejudice, independent_people
 
 borrowed(alice, pride_and_prejudice).
-borrowed(alice, '1984').
+borrowed(alice, 1984).
 borrowed(alice, independent_people).
-borrowed(bob,   war_and_peace).
-borrowed(bob,   '1984').
-borrowed(bob,   touch).
+borrowed(bob,war_and_peace).
+borrowed(bob, 1984).
+borrowed(bob, touch).
 borrowed(carol, brave_new_world).
 borrowed(carol, touch).
-borrowed(tom,   pride_and_prejudice).
-borrowed(tom,   independent_people).
+borrowed(tom, pride_and_prejudice).
+borrowed(tom, war_and_peace).
 
 
-% ---- RULES ----
+% RULES
 
 % M has borrowed a book written by A 
 author_of_borrowed_book(M, A) :-
@@ -73,32 +71,29 @@ member_list(L) :-
     list_to_set(All, L).
 
 
-% ========================
-%         PART 2
-% ========================
+% PART 2
 
-% ---- a. myprefix(L1,L2) ----
+% a. myprefix(L1,L2)
 myprefix([], _).
 myprefix([X|T1], [X|T2]) :- myprefix(T1, T2).
 
-% ---- b. num_elements(K,L) ----
+% b. num_elements(K,L)
 num_elements(0, []).
 num_elements(K, [_|L]) :-
     num_elements(K1, L),
     K is K1 + 1.
 
-% ---- c. remove_element(X,L1,L2) ----
+% c. remove_element(X,L1,L2)
 remove_element(X, [X|L1], L1).
 remove_element(X, [Y|L1], [Y|L2]) :- remove_element(X, L1, L2).
 
-% ---- d. insert_element(Elem,L1,Pos,L2) ----
+% d. insert_element(Elem,L1,Pos,L2)
 insert_element(Elem, L1, 1, [Elem|L1]).
 insert_element(Elem, [X|L1], Pos, [X|L2]) :-
-    Pos > 1,
-    Pos1 is Pos - 1,
-    insert_element(Elem, L1, Pos1, L2).
+    insert_element(Elem, L1, Pos1, L2),
+    Pos is Pos1 + 1.
 
-% ---- e. add_up_to(L1,L2)
+% e. add_up_to(L1,L2)
 add_up_to(L1, L2) :- add_up_to_helper_f(L1, L2, 0).
  
 add_up_to_helper_f([], [], _).
@@ -106,7 +101,7 @@ add_up_to_helper_f([X|L1], [S|L2], Acc) :-
     S is X + Acc,
     add_up_to_helper_f(L1, L2, S).
 
-% ---- f. remove_duplicates(L1,L2)
+% f. remove_duplicates(L1,L2)
 remove_duplicates([], []).
 remove_duplicates([X|L1], L2) :-
     member(X, L1), !,      
@@ -115,14 +110,14 @@ remove_duplicates([X|L1], L2) :-
 remove_duplicates([X|L1], [X|L2]) :- 
     remove_duplicates(L1, L2).
 
-% ---- g. split_even_odd(L, Even, Odd)
+% g. split_even_odd(L, Even, Odd)
 split_even_odd([], [], []).
 split_even_odd([X], [X], []).
 
 split_even_odd([X,Y|L], [X|Even], [Y|Odd]) :-
     split_even_odd(L, Even, Odd).
 
-% ---- h. merge_sorted(L1, L2, L3) ----
+% h. merge_sorted(L1, L2, L3)
 merge_sorted([], L2, L2).
 merge_sorted(L1, [], L1).
 merge_sorted([X|L1], [Y|L2], [X|L3]) :-
@@ -134,36 +129,32 @@ merge_sorted([X|L1], [Y|L2], [Y|L3]) :-
     merge_sorted([X|L1], L2, L3).
 
 
-% ========================
-%         PART 3
-% ========================
+% PART 3
 
-% ---- a. binaryTree(T) ----
+% a. binaryTree(T)
 binaryTree(nil).
 binaryTree(t(_, L, R)) :-
     binaryTree(L),
     binaryTree(R).
 
-% ---- b. preorder(T, L) ----
+% b. preorder(T, L)
 preorder(nil, []).
 preorder(t(X, Left, Right), L) :-
     preorder(Left, L1),
     preorder(Right, L2),
     append([X|L1], L2, L).
 
-% ---- c. leaves(T, L) ----
-leaves(nil, []).
-leaves(t(X, nil, nil), [X]).
-leaves(t(_, Left, Right), L) :-
-    (Left \= nil ; Right \= nil),
+% c. leaves(T, L)
+leaves(nil, []) :- !.
+leaves(t(X, nil, nil), [X]) :- !.
+leaves(t(_, Left, Right), Leaves) :-
     leaves(Left, L1),
     leaves(Right, L2),
-    append(L1, L2, L).
+    append(L1, L2, Leaves).
 
 
-% ========================
-%         PART 4
-% ========================
+% PART 4
+
 s([]).                                   
 s([0]).                                      
 s([1]).                                
